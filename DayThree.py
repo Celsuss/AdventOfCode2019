b40e67206d1b54033e7c2b9f09897ad46081b64c
Jens Lordén <jenslorden@gmail.com>
2019-12-12 20:21:06 +0100

    Started on day three part two

--- DayThree.py
+++ DayThree.py
@@ -128,5 +128,65 @@ def partOne():
     distances = getIntersectionDistances(intersections)
     intersection_distance = getShortestIntersectionDistance(intersections)
 
+def getDistanceBetweenNodes(node_a, node_b):
+    delta_pos = np.array(np.array(node_b) - np.array(node_a)).tolist()
+    delta_pos_sum = np.sum(delta_pos)
+    delta_pos_sum_abs = abs(int(delta_pos_sum))
+    return delta_pos_sum_abs
+
+def getStepsToIntersections(line_nodes_a, line_nodes_b, intersections):
+    for intersection in intersections:
+        line_done_a = False
+        line_done_b = False
+        line_steps_a = 0
+        line_steps_b = 0
+
+        # This don't work because the intersections is not at the same position as the nodes.
+
+        for i in range(1, len(line_nodes_a)):
+            if line_done_a == False:
+                pos_a = line_nodes_a[i]
+                line_steps_a += getDistanceBetweenNodes(pos_a, line_nodes_a[i-1])
+                if pos_a == intersection:
+                    line_done_a = True
+
+            if line_done_b == False:
+                pos_b = line_nodes_b[i]
+                line_steps_b += getDistanceBetweenNodes(pos_b, line_nodes_b[i-1])
+                if pos_b == intersection:
+                    line_done_b = True
+
+            continue
+
+        total_steps = line_steps_a + line_steps_b
+
+        continue
+
+    return 0 
+
+def testPartTwo(test_inputs):
+    for test_input in test_inputs:
+        line_nodes_a = getLineNodes(test_input[0][0])
+        line_nodes_b = getLineNodes(test_input[0][1])
+
+        intersections = getLineSegmentIntersections(line_nodes_a, line_nodes_b)
+        steps = getStepsToIntersections(line_nodes_a, line_nodes_b, intersections)
+
+        continue
+
+    return 0
+
+def partTwo():
+    input_file = 'DayThreeInput.txt'
+    test_input = [[[['R8', 'U5', 'L5', 'D3'], ['U7', 'R6', 'D4', 'L4']], 30],
+                    [[['R75','D30','R83','U83','L12','D49','R71','U7','L72'],['U62','R66','U55','R34','D71','R55','D58','R83']], 610],
+                    [[['R98','U47','R26','D63','R33','U87','L62','D20','R33','U53','R51'],['U98','R91','D20','R16','D67','R40','U7','R15','U6','R7']], 410]]
+    data = getData(input_file)
+    testPartTwo(test_input)
+
+
+    return 0
+
 if __name__ == '__main__':
-    partOne()
+    # partOne()
+    partTwo()
